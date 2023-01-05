496caf77f4227ffeed1bcf42acf462fd9f134c29
samorobo <samgdwin111@gmail.com>
2023-01-05 23:30:07 +0100

    sammy commit

--- index.js
+++ index.js
@@ -1,74 +1,59 @@
-const fs = require('fs');
+
+const express = require('express');
 const puppeteer = require('puppeteer');
 
-$('.btn4').onclick(function(){
+const app = express();
 
-    async function run() {
-        const browser = await puppeteer.launch({headless:true});
-        const page = await browser.newPage();
-    
-        const simps = ['https://www.simplyhired.com/search?q=remote&sb=dd&job', 
-                      'https://www.simplyhired.com/search?q=remote&sb=dd&pn=2&job',
-                      'https://www.simplyhired.com/search?q=remote&sb=dd&pn=3&job',
-                      'https://www.simplyhired.com/search?q=remote&sb=dd&pn=4&job',
-                      'https://www.simplyhired.com/search?q=remote&sb=dd&pn=5&job'];
-    
-        let jobs = [];
-    
-        for (let i = 0; i < simps.length; i++) {
-            const url = simps[i];
-    
-            await page.goto(url, { waitUntil: 'networkidle2' });
-    
-            const pageJobs = await page.evaluate(() =>
-                Array.from(document.querySelectorAll('#job-list .SerpJob-jobCard.card'), (e) => ({
-                    title: e.querySelector('h3').innerText,
-                    location: e.querySelector('.jobposting-location span').innerText,
-                    Description: e.querySelector('p').innerText,
-                    url: e.querySelector('.jobposting-title a').href,
-                })));
-    
-            fs.writeFileSync(`page${i}.json`, JSON.stringify(pageJobs));
-            jobs = pageJobs;
-        }
-    
-        await browser.close();
-    }
-    
-    run();
-    
+ app.use((_req, res, next) => {
+ res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
 
+  next();
+ });
+
+app.get('/search', async (req, res) => {
+  const searchQuery = req.query.q; // get the search query from the request query string
+  const pageNumber = req.query.pn; // get the page number from the request query string
+
+  // Call the run() function with the search query and page number
+  const results = await run(searchQuery, pageNumber);
+
+  // Send the results as a JSON response
+  res.json(results);
 });
-// async function run() {
-//     const browser = await puppeteer.launch({headless:true});
-//     const page = await browser.newPage();
 
-//     const simps = ['https://www.simplyhired.com/search?q=remote&sb=dd&job', 
-//                   'https://www.simplyhired.com/search?q=remote&sb=dd&pn=2&job',
-//                   'https://www.simplyhired.com/search?q=remote&sb=dd&pn=3&job',
-//                   'https://www.simplyhired.com/search?q=remote&sb=dd&pn=4&job',
-//                   'https://www.simplyhired.com/search?q=remote&sb=dd&pn=5&job'];
+app.listen(5500, () => {
+  console.log('Server listening on port 5500');
+});
 
-//     let jobs = [];
+async function run(searchQuery, pageNumber) {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
 
-//     for (let i = 0; i < simps.length; i++) {
-//         const url = simps[i];
+  // Use the search query and page number to generate the URL
+const url = `https://www.simplyhired.com/search?q=${searchQuery}&sb=dd&pn=${pageNumber}&job`;
 
-//         await page.goto(url, { waitUntil: 'networkidle2' });
+//const url = `https://www.simplyhired.com/search?q=accountant&sb=dd&pn=1&job`;
 
-//         const pageJobs = await page.evaluate(() =>
-//             Array.from(document.querySelectorAll('#job-list .SerpJob-jobCard.card'), (e) => ({
-//                 title: e.querySelector('h3').innerText,
-//                 location: e.querySelector('.jobposting-location span').innerText,
-//                 Description: e.querySelector('p').innerText,
-//                 url: e.querySelector('.jobposting-title a').href,
-//             })));
 
-//         fs.writeFileSync(`page${i}.json`, JSON.stringify(pageJobs));
-//         jobs = pageJobs;
-//     }
 
-//     await browser.close();
-// }
 
-// run();
+  await page.goto(url, { waitUntil: 'networkidle2' });
+
+  const pageJobs = await page.evaluate(() =>
+    Array.from(
+      document.querySelectorAll('#job-list .SerpJob-jobCard.card'),
+      (e) => ({
+        title: e.querySelector('h3').innerText,
+        location: e.querySelector('.jobposting-location span').innerText,
+        Description: e.querySelector('p').innerText,
+        url: e.querySelector('.jobposting-title a').href,
+      })
+    )
+  );
+
+  await browser.close();
+
+  // console.log(pageJobs)
+  return pageJobs;
+}
